c39addc68e28187762ecb749d7a88ea38720e73e
alphonso77 <762224+alphonso77@users.noreply.github.com>
2020-06-22 13:51:58 -0400

    end of step 4

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,54 @@
 import { EmptyState, Layout, Page } from '@shopify/polaris';
+import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
-const Index = () => (
-    <div>
-        <Page>
-            <Layout>
-                <EmptyState
-                    heading="Discount your products temporarily"
-                    action={{
-                        content: 'Select products',
-                        onAction: () => console.log('clicked'),
-                    }}
-                    image={img}
-                >
-                    <p>Select products to change their price temporarily.</p>
-                </EmptyState>
-            </Layout>
-        </Page>
-    </div>
-);
+// left off here 
+// https://shopify.dev/tutorials/build-a-shopify-app-with-node-and-react/build-your-user-interface-with-polaris#add-the-resource-picker
+
+class Index extends React.Component {
+    state = { open: false };
+    render() {
+        return (
+            <div>
+                <Page>
+                    <TitleBar
+                        title="Sample App"
+                        primaryAction={{
+                            content: 'Select products',
+                            onAction: () => this.setState({ open: true})
+                        }}
+                    />
+                    <ResourcePicker 
+                        resourceType="Product" 
+                        showVariants={false} 
+                        open={this.state.open} 
+                        onSelection={(resources) => this.handleSelection(resources)} 
+                        onCancel={() => this.setState({ open: false})}
+                    />
+                    <Layout>
+                        <EmptyState
+                            heading="Discount your products temporarily"
+                            action={{
+                                content: 'Select products',
+                                onAction: () => this.setState({ open: true }), 
+                            }}
+                            image={img}
+                        >
+                            <p>Select products to change their price temporarily.</p>
+                        </EmptyState>
+                    </Layout>
+                </Page>
+            </div>
+        );
+
+    }
+    handleSelection = (resources) => {
+        const idsFromResources = resources.selection.map((product) => product.id);
+        this.setState({ open: false }); 
+        console.log(resources); 
+        console.log(idsFromResources);
+    }
+}
 
 export default Index;
